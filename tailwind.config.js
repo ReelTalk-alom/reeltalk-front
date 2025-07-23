2ba125c3592bab74c1334f4bd2ef18f7ce38c843
Odyssey <odyssey409@naver.com>
2025-07-23 16:24:44 +0900

    Initialize Next.js project with TailwindCSS
    Set up a new Next.js 15 project with TypeScript, TailwindCSS, ESLint, and essential configuration files. Includes initial global styles, layout, and page components, as well as configuration for fonts, image domains, and linting.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ReelTalk_Cream: "#ECEBD6",
+      },
+    },
+  },
+  plugins: [],
+};
