bb9b6c97760b71dbb4621b92608527a3854cac5d
Odyssey <odyssey409@naver.com>
2025-07-24 01:46:52 +0900

    이미지 추가 및 커스텀 폰트 및 컬러 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-reeltalk-cream-100 h-screen">
+    <div className="bg-reeltalk-cream-base h-screen">
       <div className="flex flex-col items-center justify-center h-full">
         <h1 className="text-4xl font-bold text-black">ReelTalk</h1>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        gmarket: ["GmarketSans", "sans-serif"],
+        gmarket: ["GmarketSans"],
       },
     },
   },
