93e49b3be4942d459286e1baf6b5c5befa1505a4
Arcturus <leehyobeen747@gmail.com>
2025-08-08 16:04:33 +0900

    [#3] feat: 시작 전 구조 정리

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <div className="bg-reeltalk-cream-base h-screen">
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="text-4xl font-bold text-black">ReelTalk</h1>
-      </div>
-    </div>
+    <main>
+      <section className="bg-reeltalk-cream-base h-screen">
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-4xl font-bold text-black">ReelTalk</h1>
+          </div>
+      </section>
+
+      
+    </main>
   );
 }
