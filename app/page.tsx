b1cb3266efa8f7e3ab38b693f9017284e64b7aa2
Arcturus <leehyobeen747@gmail.com>
2025-08-08 16:15:03 +0900

    [#3] feat: 단순 크기 조정

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <main>
+    <main className="h-screen snap-y snap-mandatory">
+
+      {/* 첫 번째 메인 페이지 */}
       <section className="bg-reeltalk-cream-base h-screen">
           <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-4xl font-bold text-black">ReelTalk</h1>
+            <h1 className="text-9xl font-bold text-black">REELTALK</h1>
           </div>
       </section>
 
-      
+
     </main>
   );
 }
